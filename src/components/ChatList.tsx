87da877d8ee8b115420b10e2db0c3aff05474b49
kimhw7 <khw786678@gmail.com>
2023-02-25 23:18:32 +0900

    add error note

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -41,7 +41,7 @@ function ChatList(props: any) {
       model: "text-davinci-003",
       prompt: question,
       temperature: 0.7,
-      max_tokens: 48,
+      max_tokens: 100,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
